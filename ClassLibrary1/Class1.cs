055f2fcb7fab30478ef3d419b7a0fed52cf155c7
shoaibalirts <shoaibali.rts@gmail.com>
2025-04-18 11:55:13 +0200

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public delegate void MyDelegateType(int a, int b);
+
+        // publisher
+        public class Publisher
+        {
+            // private delegate
+            private MyDelegateType myDelegate;
+
+            // step1: create event (just like we create a property inside a class)
+            public event MyDelegateType myEvent
+            {
+                add
+                {
+                    myDelegate += value;
+                }
+                remove
+                {
+                    myDelegate -= value;
+                }
+            }
+        }
+    }
+}
